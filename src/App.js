255470e5458e43815848d1299043252548a95d13
Roy Bae <hidinkyu@macbook-pro.lan>
2022-11-06 18:47:02 -0800

    basic outline

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from'react';
+const apo = {
+  key: 'ca89ecbeb40b4fc4b8df945af5958e17',
+  base: 'https://api.openweathermap.org/data/3.0'
+}
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
